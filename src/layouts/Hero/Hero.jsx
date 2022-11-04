516ec8ecb18bad5af12333a0e9eb469f8b06b80a
fernandogutz <03fernandogutierrez@gmail.com>
2022-11-04 06:06:11 -0300

    Responsive sección Hero

--- src/layouts/Hero/Hero.jsx
+++ src/layouts/Hero/Hero.jsx
@@ -11,8 +11,8 @@ const Hero = () => {
                     <h1 className="hero__title">Frontend <span>Developer</span></h1>
                     {/*  <p className="hero__subtitle">Enfocado en la creación de productos y experiencias digitales para proyectos que me inspiran.</p>*/}
                     <div className="hero__buttons">
-                        <a className="hero__btn btn-primary"href="#sobre-mi">Mira mis proyectos</a>
-                        <a className="hero__btn btn-secondary hero__btn--contact" href="#contacto">Contáctame</a>
+                        <a className="hero__btn btn-primary" href="#proyectos">Ver Proyectos</a>
+                        <a className="hero__btn btn-secondary hero__btn--contact" href="#contacto">Contacto</a>
                     </div>
                 </section>
                 <div className="hero__img">
